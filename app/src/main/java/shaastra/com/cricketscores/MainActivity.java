580994077b4d69067fdc2c10d82d20b0a49f5018
rajatvd <rajatvd@gmail.com>
2017-05-18 17:48:01 +0530

    Added basic functionality to Match class, and listed out some example matches

--- app/src/main/java/shaastra/com/cricketscores/MainActivity.java
+++ app/src/main/java/shaastra/com/cricketscores/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.Arrays;
+import java.util.Vector;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,12 +15,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] myStringArray = {"hi","bye","ooooooooo big string","hi","bye","ooooooooo big string","hi","bye","ooooooooo big string","hi","bye","ooooooooo big string","hi","bye","ooooooooo big string","hi","bye","ooooooooo big string"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, myStringArray);
+
+
+
+        ArrayAdapter<String> matches = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1);
+
+        initMatches(matches);
 
         ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        listView.setAdapter(matches);
 
+    }
+
+    // Setup some example matches to display
+    private void initMatches(ArrayAdapter m){
+        Match m1 = new Match("KKR", "RCB");
+        m1.team1Score = "178/3";
+        m1.team1Overs = "20";
+        m1.team2Score = "180/5";
+        m1.team2Overs = "18.5";
+        m.add(m1);
+        Match m2 = new Match("SRH", "MI");
+        m2.team1Score = "130/8";
+        m2.team1Overs = "20";
+        m2.team2Score = "111/5";
+        m2.team2Overs = "20";
+        m.add(m2);
+        Match m3 = new Match("DD", "KKR");
+        m3.team1Score = "90/10";
+        m3.team1Overs = "13";
+        m3.team2Score = "91/2";
+        m3.team2Overs = "15.3";
+        m.add(m3);
+        Match m4 = new Match("SRH", "RCB");
+        m4.team1Score = "178/8";
+        m4.team1Overs = "20";
+        m4.team2Score = "130/9";
+        m4.team2Overs = "20";
+        m.add(m4);
     }
 }
--- app/src/main/java/shaastra/com/cricketscores/Match.java
+++ app/src/main/java/shaastra/com/cricketscores/Match.java
@@ -5,5 +5,26 @@ package shaastra.com.cricketscores;
  */
 
 public class Match {
+    String team1, team1Score, team1Overs,
+            team2, team2Score, team2Overs;
+
+    public Match(String team11, String team22){
+        team1 = team11;
+        team2 = team22;
+        team1Score = "0/0";
+        team2Score = "0/0";
+        team1Overs = "0";
+        team2Overs = "0";
+    }
+
+    public boolean didTeam1Win(){
+        int team1runs = Integer.parseInt(team1Score.split("/")[0]);
+        int team2runs = Integer.parseInt(team2Score.split("/")[0]);
+        return team1runs>team2runs;
+    }
+
+    public String toString(){
+        return team1 + " vs " + team2;
+    }
 
 }
