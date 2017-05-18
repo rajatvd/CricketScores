085b29a75c1f0fabe280982e28a24235afdd99f2
rajatvd <rajatvd@gmail.com>
2017-05-18 18:18:25 +0530

    Added detailed description functionality

--- app/src/main/java/shaastra/com/cricketscores/MainActivity.java
+++ app/src/main/java/shaastra/com/cricketscores/MainActivity.java
@@ -2,9 +2,13 @@ package shaastra.com.cricketscores;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.Console;
 import java.util.Arrays;
 import java.util.Vector;
 
@@ -21,10 +25,27 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> matches = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1);
 
+
         initMatches(matches);
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(matches);
+
+
+        AdapterView.OnItemClickListener mMessageClickedHandler = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                Log.i("test",parent.getAdapter().getItem(position)+"");
+                ArrayAdapter mats = (ArrayAdapter) parent.getAdapter();
+                Match m = (Match) mats.getItem(position);
+                m.showDetails = !m.showDetails;
+                Log.i("match", m.showDetails + "");
+                parent.setAdapter(mats);
+                // (ArrayAdapter)(parent.getAdapter()).getItem(position).showDetails = !(ArrayAdapter)(parent.getAdapter()).getItem(position).showDetails;
+            }
+        };
+
+        listView.setOnItemClickListener(mMessageClickedHandler);
+
 
     }
 
--- app/src/main/java/shaastra/com/cricketscores/Match.java
+++ app/src/main/java/shaastra/com/cricketscores/Match.java
@@ -8,6 +8,8 @@ public class Match {
     String team1, team1Score, team1Overs,
             team2, team2Score, team2Overs;
 
+    public boolean showDetails;
+
     public Match(String team11, String team22){
         team1 = team11;
         team2 = team22;
@@ -18,12 +20,20 @@ public class Match {
     }
 
     public boolean didTeam1Win(){
+        // Compare the runs scored by each team to determine if team 1 won or not
         int team1runs = Integer.parseInt(team1Score.split("/")[0]);
         int team2runs = Integer.parseInt(team2Score.split("/")[0]);
         return team1runs>team2runs;
     }
 
     public String toString(){
+        if(showDetails){
+            String s =  team1 + " vs " + team2;
+            s+="\n"+team1Score+"\t \t"+ team2Score;
+            s+="\n"+team1Overs+"\t \t"+ team2Overs;
+            s+="\n"+ (didTeam1Win()?team1:team2) + " Won";
+            return s;
+        }
         return team1 + " vs " + team2;
     }
 
